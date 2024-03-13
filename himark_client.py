6fdff9c24e44c817a2ebea85ee63a5a9f1cbdf58
Nicholas hopkins <hopkins.n3@gmail.com>
2024-03-13 13:44:49 -0700

    textual interface now accepts input from the line, and adds broadcasted messages from the server to the message box

--- himark_client.py
+++ himark_client.py
@@ -18,67 +18,84 @@ SERVER_INIT_CONNECTION_RESPONSE = "HEYJOHNNY"
 
 WS_SERVER_ADDR = f"ws://{ip}:{port}/ws_connect"
 CONNECT_SERVER_ADDR = f"http://{ip}:{port}/connection_attempt"
+class Client_Connection:
 
-GLOBAL_WS = None
-
-async def wait_for_messages(textual_obj):
-    while True:
-        recv = await GLOBAL_WS.recv() #wait for a message
-
-        textual_obj.query_one('#message_box').append(ListItem(Label(recv)))
-
-async def send_message(textual_obj, event):
-    txt = textual_obj.query_one(event)
-
-    if txt.value == "exit": #tell the server we are disconnecting
-        await GLOBAL_WS.close()
-    else:
-        await GLOBAL_WS.send(txt.value)
-
-
-
-async def main(textual_obj):
-    try:
-        #test to see if a himark server is running on the given ip:port
+    def __init__(self, textual_obj):
+        #attempt connection. if it works, continue. otherwise sys exit
         response = requests.post(CONNECT_SERVER_ADDR, json={"arg1":CLIENT_INIT_CONNECTION_MESSAGE})
 
         if(response.json() != {'arg2':SERVER_INIT_CONNECTION_RESPONSE}):
             sys.exit(f"There is no himark server running on {ip}:{port}")
 
-        async with websockets.connect(WS_SERVER_ADDR) as websocket:
+        self.ws = 0
+        self.textual_obj = textual_obj
 
-            GLOBAL_WS = websocket
+    async def wait_for_messages(self):
+        try:
+            while True:
+                recv = await self.ws.recv() #wait for a message
 
-            asyncio.create_task(wait_for_messages(textual_obj)) #task for waiting for messages
+                self.textual_obj.query_one('#message_box').append(ListItem(Label(recv)))
+        except WebSocketException:
+            sys.exit("WebSocket error occured")
+        except asyncio.CancelledError:
+            sys.exit("User cancelled")
 
-    except WebSocketException:
-        #raised when user types 'exit' to quit program
-        sys.exit(1)
-    except ConnectionClosed:
-        sys.exit("Connection closed")
-    except ConnectionError:
-        sys.exit(f"No service is running on {ip}:{port}")
-    except KeyboardInterrupt:
-        sys.exit("Program killed by user")
-    except SystemExit:
-        sys.exit("Program killed by user")
-    except asyncio.CancelledError:
-        sys.exit("There was an error cancelling an asynchronous routine")
+    async def send_message(self, txt):
+        if txt == "exit": #tell the server we are disconnecting
+            raise ConnectionClosed
+        else:
+            await self.ws.send(txt)
+
+
+    async def main(self):
+        try:
+
+            async with websockets.connect(WS_SERVER_ADDR) as self.ws:
+
+                await asyncio.create_task(self.wait_for_messages()) #task for waiting for messages
+
+        except WebSocketException:
+            #raised when user types 'exit' to quit program
+            sys.exit()
+        except ConnectionClosed:
+            sys.exit("Connection closed")
+        except ConnectionError:
+            sys.exit(f"No service is running on {ip}:{port}")
+        except KeyboardInterrupt:
+            sys.exit("Program killed by user")
+        except SystemExit:
+            sys.exit("Program killed by user")
+        except asyncio.CancelledError:
+            sys.exit("There was an error cancelling an asynchronous routine")
 
 
 class Client(App):
     CSS_PATH = "client.tcss"
+    LOG_FILE = "himark.log"
 
     @on(Input.Submitted)
-    async def on_input_submitted(self) -> None:
-        send_message(self, Input)
+    async def client_input(self) -> None:
+        input = self.query_one(Input) #get the input
+
+        try:
+            await self.c_conn.send_message(input.value) #send message function from client connection
+        except:
+            sys.exit("Connection Closed by user")
+
+        input.value = "" #clear input
 
     def compose(self) -> ComposeResult:
         yield ListView(classes="box", id="message_box") #box to put messages in
         yield ListView(classes="names", id="name_box") #box to put connected users in
-        yield Input(placeholder=">", type="text") #box to capture input. on submit we call  send_message
+        yield Input(placeholder=">", type="text") #box to capture input. on submit we call send_message
 
-        asyncio.create_task(main(self))
+        self.c_conn = Client_Connection(self)
+
+        try:
+            asyncio.create_task(self.c_conn.main()) #run the main function of the client connection
+        except:
+            print("f")
 
 if __name__ == "__main__":
     client = Client()
